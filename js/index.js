6d87f50b80ee54ccd6c448446c940f7028fc4d24
Gleici Pereira <gpcaresdev@gmail.com>
2024-08-22 20:05:06 -0500

    js file placement adjustment

--- js/index.js
+++ js/index.js
@@ -1,40 +1,37 @@
 
-/* Navigation */
+/****** Navigation ******/
 
-document.addEventListener("DOMContentLoaded", () => {
+const navLinks = document.querySelectorAll(".nav-link");
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navTogglerIcon = document.getElementById("toggler-icon");
 
-    const navLinks = document.querySelectorAll(".nav-link");
-    const navbarToggler = document.querySelector(".navbar-toggler");
-    const navTogglerIcon = document.getElementById("toggler-icon");
-
-    // Close toggler menu when links are clicked
-    navLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            // Check if the navbar is currently expanded
-            if (navbarToggler.getAttribute("aria-expanded") === "true") {
-                // Close it
-                navbarToggler.click();
-            }
-        });
-    });
-
-
-    // Change toggler icons when menu toggler is open/closed
-    navbarToggler.addEventListener("click", () => {
-
-        // Initially, toggle is closed/collapsed (aria-expanded === "false")
-        const isCollapsed = navbarToggler.getAttribute("aria-expanded") === "false";
-        
-        if (!isCollapsed) {
-            navTogglerIcon.classList.add("open");
-        }
-        else{
-            navTogglerIcon.classList.remove("open");
+// Close toggler menu when links are clicked
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        // Check if the navbar is currently expanded
+        if (navbarToggler.getAttribute("aria-expanded") === "true") {
+            // Close it
+            navbarToggler.click();
         }
     });
-
-
 });
+
+
+// Change toggler icons when menu toggler is open/closed
+navbarToggler.addEventListener("click", () => {
+
+    // Initially, toggle is closed/collapsed (aria-expanded === "false")
+    const isCollapsed = navbarToggler.getAttribute("aria-expanded") === "false";
+    
+    if (!isCollapsed) {
+        navTogglerIcon.classList.add("open");
+    }
+    else{
+        navTogglerIcon.classList.remove("open");
+    }
+});
+
+
 
 
 
